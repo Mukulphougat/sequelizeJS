47dcc39539973f1e5382631cc5706603fc43f96b
Mukul <mukulphoughat@gmail.com>
2023-12-14 15:38:20 +0530

    added comment in main.js and added .idea dir in gitignore

--- associations/main.js
+++ associations/main.js
@@ -5,6 +5,7 @@ import Capital from '../models/CapitalModel.js'
 try {
   await sequelize.authenticate()
   // One-To-One Associations We Have To Always Define This
+
   Country.hasOne(Capital) // Setting One-To-One Relation
   await sequelize.sync()
   const allData = await Country.findAll({ include: Capital })
